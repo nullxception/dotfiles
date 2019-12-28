c18c661c305e0d4ecabda59c4dcadf81ac16cc01
Nauval Rizky <enuma.alrizky@gmail.com>
2019-12-28 09:56:07 +0900

    gitsetup: setup credential helper cache
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>

--- gitsetup.sh
+++ gitsetup.sh
@@ -4,8 +4,10 @@ echo "Configuring git"
 echo "Previous user was : $(git config --global user.name) <$(git config --global user.email)>"
 read -p 'Name: ' name
 read -p 'email: ' email
+read -p 'Git credential cache time (in hours): ' $pwdtime
 git config --global user.name "$name"
 git config --global user.email "$email"
+[ $pwtime -gt 0 ] && git config --global credential.helper "cache --timeout $((3600*$pwtime))"
 git config --global merge.renamelimit 999999
 git config --global diff.renameLimit 999999
 git config --global alias.merge 'merge --no-ff'
