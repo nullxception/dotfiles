9649ceafaea060a19c6b370c341e0ae2b392728b
Nauval Rizky <enuma.alrizky@gmail.com>
2019-12-30 10:11:38 +0900

    pywal: post-launch: set wallpaper to KDE Plasma
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>

--- userhome/files/.config/wal/post-launch.sh
+++ userhome/files/.config/wal/post-launch.sh
@@ -3,7 +3,7 @@ IMG="$(cat ~/.cache/wal/wal)"
 
 # For awesomewm -- only to send refresh signal
 if command -v awesome-client 2>&1 /dev/null; then
-    echo 'awesome.emit_signal("pywal::apply")' | awesome-client
+  echo 'awesome.emit_signal("pywal::apply")' | awesome-client
 fi
 
 # For XFCE4
@@ -14,5 +14,19 @@ if command -v xfconf-query 2>&1 /dev/null; then
 fi
 
 # For GNOME
-gsettings set org.gnome.desktop.background picture-uri "file://$IMG"
+if command -v gsettings 2>&1 /dev/null; then
+  gsettings set org.gnome.desktop.background picture-uri "file://$IMG"
+fi
 
+if command -v qdbus 2>&1 /dev/null; then
+  # For KDE
+  qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript '
+    var a=desktops();
+    for(i=0;i<a.length;i++) {
+      b=a[i];
+      b.wallpaperPlugin="org.kde.image";
+      b.currentConfigGroup=Array("Wallpaper","org.kde.image","General");
+      b.writeConfig("Image","'$IMG'");
+    }
+  '
+fi
