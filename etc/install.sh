ad412c8e47e0f15c74bb688ead83a569d14bf9cb
Nauval Rizky <enuma.alrizky@gmail.com>
2020-04-12 15:46:13 +0900

    treewide: Refactor structures
    Now all modules are structured without "/files/".
    Installation script will be simplified with PKGBUILD-style
    
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>

--- etc/install.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-CURR=$(realpath $(dirname $0))
-TARGET=/etc
-
-echo "installing files to $(realpath $TARGET)"
-sudo cp -avf $CURR/files/. $TARGET/
--- userhome/install.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-CURR=$(realpath $(dirname $0))
-TARGET=$HOME
-
-echo "installing files to $(realpath $TARGET)"
-cp -avf $CURR/files/. $TARGET/
