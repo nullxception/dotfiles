33e51250e567bf87bc4071b57aa52f8ad4a87d4b
Nauval Rizky <enuma.alrizky@gmail.com>
2020-04-15 23:54:20 +0900

    firefox: add better scrolling tweaks
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>

--- firefox/user.js
+++ firefox/user.js
@@ -12,3 +12,19 @@ user_pref("widget.content.allow-gtk-dark-theme", true);
 user_pref("widget.content.gtk-theme-override", "Adwaita"); // or any other light themes
 // Fix HTML5 video tearing
 user_pref("layers.acceleration.force-enabled", true);
+// Scrolling Tweaks
+user_pref("general.smoothScroll.lines.durationMaxMS", 125);
+user_pref("general.smoothScroll.lines.durationMinMS", 125);
+user_pref("general.smoothScroll.mouseWheel.durationMaxMS", 200);
+user_pref("general.smoothScroll.mouseWheel.durationMinMS", 100);
+user_pref("general.smoothScroll.msdPhysics.enabled", true);
+user_pref("general.smoothScroll.other.durationMaxMS", 125);
+user_pref("general.smoothScroll.other.durationMinMS", 125);
+user_pref("general.smoothScroll.pages.durationMaxMS", 125);
+user_pref("general.smoothScroll.pages.durationMinMS", 125);
+user_pref("mousewheel.min_line_scroll_amount", 15);
+user_pref("mousewheel.system_scroll_override_on_root_content.enabled", true);
+user_pref("mousewheel.system_scroll_override_on_root_content.horizontal.factor", 175);
+user_pref("mousewheel.system_scroll_override_on_root_content.vertical.factor", 175);
+user_pref("toolkit.scrollbox.horizontalScrollDistance", 6);
+user_pref("toolkit.scrollbox.verticalScrollDistance", 2);
