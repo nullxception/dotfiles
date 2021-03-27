89d511d279e7a1ff05b04e3b335cb221483dd421
Nauval Rizky <enuma.alrizky@gmail.com>
2021-03-27 14:00:12 +0700

    pwsh: remove posh-git installer

--- powershell/.module-data.ps1
+++ powershell/.module-data.ps1
@@ -1,8 +1,1 @@
 $InstallTarget = Split-Path $profile
-
-function PreInstall-Dotfiles {
-    if (!(Get-Module posh-git -ErrorAction SilentlyContinue)) {
-        "posh-git not installed, installing posh-git powershell module for the Current User..."
-        Install-Module posh-git -Scope CurrentUser -AllowPrerelease -Force
-    }
-}
