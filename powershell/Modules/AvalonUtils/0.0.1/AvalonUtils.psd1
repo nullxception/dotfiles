fa2d68f8163932cb18bb80e5b1127176f751cd46
Nauval Rizky <enuma.alrizky@gmail.com>
2021-03-30 13:26:01 +0700

    pwsh: add my powershell utility set

---
+++ powershell/Modules/AvalonUtils/0.0.1/AvalonUtils.psd1
@@ -0,0 +1,16 @@
+@{
+    RootModule        = 'AvalonUtils.psm1'
+    ModuleVersion     = '0.0.1'
+    GUID              = 'f8cee14d-de54-458f-9407-a8e8729194c6'
+    Author            = 'Nauval'
+    CompanyName       = 'Chaldeastudio'
+    Description       = 'Set of utilities for the chaldeans'
+    Copyright         = '(c) Nauval. All rights reserved.'
+    FunctionsToExport = @("Register-Path", "Update-CurrentEnv")
+    CmdletsToExport   = @()
+    VariablesToExport = '*'
+    AliasesToExport   = @()
+    PrivateData       = @{
+        PSData = @{}
+    }
+}
---
+++ powershell/Modules/AvalonUtils/0.0.1/AvalonUtils.psm1
@@ -0,0 +1,39 @@
+function Register-Path {
+    param (
+        [Parameter(Mandatory = $true)] $At,
+        [System.EnvironmentVariableTarget] $Scope = 'User'
+    )
+    $CurrentPath = [Environment]::GetEnvironmentVariable("Path", $Scope)
+    if ($CurrentPath.Split(";") -notcontains "$At") {
+        [Environment]::SetEnvironmentVariable("Path", $CurrentPath + "$At;", $Scope)
+    }
+
+    # Finally, add it to current session
+    if ($env:Path.Split(";") -notcontains $At) {
+        $env:Path += ";$At"
+    }
+}
+
+function Update-CurrentEnv {
+    $ignored = @('USERNAME', 'PSModulePath', 'Path')
+
+    'Process', 'Machine', 'User' | ForEach-Object {
+        $scope = $_
+        switch ($Scope) {
+            'Process' { $names = Get-ChildItem Env:\ | Select-Object -ExpandProperty Key }
+            'Machine' { $names = Get-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment' | Select-Object -ExpandProperty Property }
+            'User' { $names = Get-Item 'HKCU:\Environment' | Select-Object -ExpandProperty Property }
+        }
+        $names | Where-Object -FilterScript { $ignored -notcontains "$_" } | ForEach-Object {
+            $fromSession = Get-Item "Env:\${_}" | Select-Object -ExpandProperty Value
+            $fromReg = [Environment]::GetEnvironmentVariable($_, $scope)
+            if ($fromReg -ne $fromSession) {
+                Set-Item "Env:\${_}" -Value $fromReg
+            }
+        }
+    }
+
+    $env:Path = 'Machine', 'User' | ForEach-Object {
+        [Environment]::GetEnvironmentVariable('Path', $_) -split ';'
+    } | Select-Object -Unique | Join-String -Separator ";"
+}
