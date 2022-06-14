2caa23c05196264cf3820dcff91d7a6d77b0b3ba
Nauval Rizky <enuma.alrizky@gmail.com>
2022-06-15 06:46:28 +0700

    install.ps1: use Resolve-Path instead

--- install.ps1
+++ install.ps1
@@ -12,39 +12,38 @@ $ModuleData = ".module-data.ps1"
 
 function Install-Mod($ModulePath) {
     $ModuleName = Split-Path $ModulePath -Leaf
+    $ModuleRPath = Resolve-Path $ModulePath\$ModuleData -ErrorAction SilentlyContinue
+    if (![System.IO.File]::Exists($ModuleRPath)) {
+        Return "Module $ModuleName does not exists or has no $ModuleData"
+    }
 
-    if ([System.IO.File]::Exists("$ModulePath\$ModuleData")) {
-        . "$ModulePath\$ModuleData"
-        if (Get-Command 'PreInstall-Dotfiles' -errorAction SilentlyContinue) {
-            PreInstall-Dotfiles
-        }
-        if (Get-Command 'Install-Dotfiles' -errorAction SilentlyContinue) {
-            Install-Dotfiles
-        }
-        else {
-            Write-Output "installing module $ModuleName to $Installtarget"
-            if (![System.IO.Directory]::Exists($Installtarget)) {
-                New-Item -ItemType Directory -Path $Installtarget
-            }
-            Get-ChildItem -Path $ModulePath -Exclude $ModuleData, .module-data.bash | Copy-Item -Destination $Installtarget -Recurse -Force
-        }
-        if (Get-Command 'PostInstall-Dotfiles' -errorAction SilentlyContinue) {
-            PostInstall-Dotfiles
-        }
-
-        # Unregister modules functions
-        if (Get-Command 'PreInstall-Dotfiles' -errorAction SilentlyContinue) {
-            Remove-Item -Path Function:\PreInstall-Dotfiles
-        }
-        if (Get-Command 'Install-Dotfiles' -errorAction SilentlyContinue) {
-            Remove-Item -Path Function:\Install-Dotfiles
-        }
-        if (Get-Command 'PostInstall-Dotfiles' -errorAction SilentlyContinue) {
-            Remove-Item -Path Function:\PostInstall-Dotfiles
-        }
+    . $ModuleRPath
+    if (Get-Command 'PreInstall-Dotfiles' -errorAction SilentlyContinue) {
+        PreInstall-Dotfiles
+    }
+    if (Get-Command 'Install-Dotfiles' -errorAction SilentlyContinue) {
+        Install-Dotfiles
     }
     else {
-        "Module $ModuleName does not exists or has no $ModuleData"
+        Write-Output "installing module $ModuleName to $Installtarget"
+        if (![System.IO.Directory]::Exists($Installtarget)) {
+            New-Item -ItemType Directory -Path $Installtarget
+        }
+        Get-ChildItem -Path $ModulePath -Exclude $ModuleData, .module-data.bash | Copy-Item -Destination $Installtarget -Recurse -Force
+    }
+    if (Get-Command 'PostInstall-Dotfiles' -errorAction SilentlyContinue) {
+        PostInstall-Dotfiles
+    }
+
+    # Unregister modules functions
+    if (Get-Command 'PreInstall-Dotfiles' -errorAction SilentlyContinue) {
+        Remove-Item -Path Function:\PreInstall-Dotfiles
+    }
+    if (Get-Command 'Install-Dotfiles' -errorAction SilentlyContinue) {
+        Remove-Item -Path Function:\Install-Dotfiles
+    }
+    if (Get-Command 'PostInstall-Dotfiles' -errorAction SilentlyContinue) {
+        Remove-Item -Path Function:\PostInstall-Dotfiles
     }
 }
 
