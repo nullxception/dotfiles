ca980e23309cbf12bda6d0e8cc0b222b0ad5d0e3
Nauval Rizky <enuma.alrizky@gmail.com>
2022-06-29 16:13:29 +0700

    cleanup fonts setup

--- home/.module-data.bash
+++ home/.module-data.bash
@@ -1,16 +1,2 @@
 #!/bin/bash
 module_target="$HOME"
-
-module_postinstall() {
-    # Configure KDE Fonts
-    if command -v kwriteconfig5 > /dev/null 2>&1; then
-        kwriteconfig5 --group General --key XftAntialias true
-        kwriteconfig5 --group General --key XftHintStyle hintslight
-        kwriteconfig5 --group General --key XftSubPixel rgb
-    fi
-    # Configure GNOME Fonts
-    if command -v dconf > /dev/null 2>&1; then
-        dconf write /org/gnome/settings-daemon/plugins/xsettings/antialiasing "'rgba'"
-        dconf write /org/gnome/settings-daemon/plugins/xsettings/hinting "'slight'"
-    fi
-}
