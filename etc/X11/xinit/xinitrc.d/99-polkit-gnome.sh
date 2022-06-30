b4734bc531963bddcc8421a623b3883a61440b7c
Nauval Rizky <enuma.alrizky@gmail.com>
2022-06-30 14:40:30 +0700

    {hw,etc}: cleanup old stuff

--- etc/X11/xinit/xinitrc.d/99-polkit-gnome.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-POLKIT_LIB=/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
-case "${DESKTOP_SESSION-}" in
-    gnome*) # Done by gnome-settings-daemon
-        ;;
-    *)
-        exec $POLKIT_LIB &
-        ;;
-esac
