d3a3f331ac3f2abac8707785ca4e4ef44f1c586d
Nauval Rizky <enuma.alrizky@gmail.com>
2022-07-21 15:33:05 +0700

    pwsh: update setup scripts

--- windows/pwsh/Scripts/setup-env.ps1
+++ windows/pwsh/Scripts/setup-env.ps1
@@ -1,57 +1,69 @@
 #
 # nullxception's desktop enviroment setup
-# usage:
-# ./install.ps1 <module-name>
 #
 
-if (!(Get-Command scoop)) {
+if (!(Get-Command scoop -ErrorAction SilentlyContinue)) {
     Invoke-WebRequest -useb get.scoop.sh | Invoke-Expression
 }
+else {
+    "scoop is already installed"
+}
 
-winget install --scope machine -e gerardog.gsudo
-Update-CurrentEnv
-gsudo config CacheMode Auto
+function on_winget($com, $id, $then = {}) {
+    if (!(Get-Command $com -ErrorAction SilentlyContinue)) {
+        winget install --scope machine -e $id
+        Update-CurrentEnv
+        & $then
+    }
+    else {
+        "$com is already installed"
+    }
+}
 
-winget install --scope machine -e 7zip.7zip
-winget install --scope machine -e GoLang.Go
-winget install --scope machine -e Lexikos.AutoHotkey
-winget install --scope machine -e Python.Python.3
+function on_scoop($com, $id, $then = {}) {
+    if (!(Get-Command $com -ErrorAction SilentlyContinue)) {
+        scoop install $id
+        Update-CurrentEnv
+        & $then
+    }
+    else {
+        "$com is already installed"
+    }
+}
 
-# Setup aria2 for the scoop downloader
-scoop install aria2
-scoop config aria2-enabled true
 
-# Setup buckets
+on_winget gsudo gerardog.gsudo {
+    gsudo config CacheMode Auto
+}
+
+on_winget git Git.Git {
+    # Cleanup Git context menus
+    gsudo Remove-Item -Force -ErrorAction SilentlyContinue -Path HKCR:\Directory\shell\git_gui
+    gsudo Remove-Item -Force -ErrorAction SilentlyContinue -Path HKCR:\Directory\shell\git_shell
+    gsudo Remove-Item -Force -ErrorAction SilentlyContinue -Path HKCR:\LibraryFolder\background\shell\git_gui
+    gsudo Remove-Item -Force -ErrorAction SilentlyContinue -Path HKCR:\LibraryFolder\background\shell\git_shell
+    gsudo Remove-Item -Force -ErrorAction SilentlyContinue -Path HKLM:\SOFTWARE\Classes\Directory\background\shell\git_gui
+    gsudo Remove-Item -Force -ErrorAction SilentlyContinue -Path HKLM:\SOFTWARE\Classes\Directory\background\shell\git_shell
+}
+
+on_winget 7z 7zip.7zip
+on_winget AutoHotkey Lexikos.AutoHotkey
+on_winget python3 Python.Python.3
+
+on_scoop aria2c aria2 {
+    scoop config aria2-enabled true
+}
+
 scoop bucket add extras
 scoop bucket add nerd-fonts
 scoop bucket add retools https://github.com/TheCjw/scoop-retools.git
 
-# Apps, fonts, runtime, etc
-gsudo scoop install `
-    bat `
-    jadx `
-    less `
-    lsd `
-    nssm `
-    neovim `
-    ripgrep
+on_scoop bat bat
+on_scoop jadx jadx
+on_scoop less less
+on_scoop lsd lsd
+on_scoop nssm nssm
+on_scoop nvim neovim
+on_scoop rg ripgrep
 
 gsudo scoop install --global JetBrainsMono-NF
-gsudo "$(scoop prefix python)\install-pep-514.reg"
-
-# Setup git and openssh
-gsudo scoop install -g openssh git
-gsudo "$env:ProgramData\scoop\apps\openssh\current\install-sshd.ps1"
-gsudo "Get-Service ssh-agent | Set-Service -StartupType Automatic -PassThru | Start-Service"
-
-# Register things to $PATH
-gsudo Register-Path -Scope Machine "$env:ProgramFiles\7-Zip"
-gsudo Register-Path -Scope Machine "$env:ProgramFiles\AutoHotkey"
-
-# Housekeeping context menus
-gsudo Remove-Item -Force -ErrorAction SilentlyContinue -Path HKCR:\Directory\shell\git_gui
-gsudo Remove-Item -Force -ErrorAction SilentlyContinue -Path HKCR:\Directory\shell\git_shell
-gsudo Remove-Item -Force -ErrorAction SilentlyContinue -Path HKCR:\LibraryFolder\background\shell\git_gui
-gsudo Remove-Item -Force -ErrorAction SilentlyContinue -Path HKCR:\LibraryFolder\background\shell\git_shell
-gsudo Remove-Item -Force -ErrorAction SilentlyContinue -Path HKLM:\SOFTWARE\Classes\Directory\background\shell\git_gui
-gsudo Remove-Item -Force -ErrorAction SilentlyContinue -Path HKLM:\SOFTWARE\Classes\Directory\background\shell\git_shell
--- windows/pwsh/Scripts/setup-git.ps1
+++ windows/pwsh/Scripts/setup-git.ps1
@@ -5,6 +5,7 @@
 
 $CurrentName = git config --global user.name
 $CurrentEmail = git config --global user.email
+
 Write-Host "Configuring git"
 Write-Host "Previous user was : $CurrentName <$CurrentEmail>"
 
@@ -19,20 +20,5 @@ git config --global diff.renameLimit 999999
 git config --global alias.merge 'merge --no-ff'
 git config --global alias.onelog 'log --oneline --pretty=format:\"%h # %ai %s\"'
 git config --global alias.onebase 'log --oneline --pretty=format:\"GIT_COMMITTER_DATE=\\\"%ci\\\" GIT_AUTHOR_DATE=\\\"%ai\\\" git cherry-pick %h # %s\"'
-git config --global core.sshCommand $((Get-Command ssh).Source).Replace("\", "\\")
-
-gsudo 'Get-Service ssh-agent | Set-Service -StartupType Automatic -PassThru | Start-Service'
-
-Write-Host "Getting wsl-ssh-agent"
-$Url = 'https://github.com/rupor-github/wsl-ssh-agent/releases/latest/download/wsl-ssh-agent.zip'
-$ZipFile = $env:TEMP + $(Split-Path -Path $Url -Leaf)
-$Destination = 'C:\Libraries\wsl-tools'
-
-Invoke-WebRequest -Uri $Url -OutFile $ZipFile
-$ExtractShell = New-Object -ComObject Shell.Application
-$Files = $ExtractShell.Namespace($ZipFile).Items()
-$ExtractShell.NameSpace($Destination).CopyHere($Files)
-gsudo Start-Process $Destination
-gsudo Register-Path $Destination -Scope Machine
 
 Write-Host "done!"
