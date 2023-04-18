50b945ed753dbd4e8dde2fc963154e0753b81ead
Nauval Rizky <enuma.alrizky@gmail.com>
2023-04-19 05:51:01 +0700

    firefox: update tweaks

--- firefox/.install.ps1
+++ firefox/.install.ps1
@@ -1,22 +1,24 @@
+$FFDir = "$env:APPDATA\Mozilla\Firefox"
+
 function Get-IniContent ($filePath) {
     $ini = @{}
     switch -regex -file $FilePath {
-        "^\[(.+)\]" # Section
-        {
+        "^\[(.+)\]" {
+            # Section
             $section = $matches[1]
             $ini[$section] = @{}
             $CommentCount = 0
         }
-        "^(;.*)$" # Comment
-        {
+        "^(;.*)$" {
+            # Comment
             $value = $matches[1]
             $CommentCount = $CommentCount + 1
             $name = "Comment" + $CommentCount
             $ini[$section][$name] = $value
         }
-        "(.+?)\s*=(.*)" # Key
-        {
-            $name,$value = $matches[1..2]
+        "(.+?)\s*=(.*)" {
+            # Key
+            $name, $value = $matches[1..2]
             $ini[$section][$name] = $value
         }
     }
@@ -24,16 +26,27 @@ function Get-IniContent ($filePath) {
 }
 
 
+function Install-Files($DotDir, $Target) {
+    $Dest = "$FFDir\${Target}"
+
+    New-Item -Type Directory $Dest -Force | Out-Null
+    ":: Install user.js to ${Target}"
+    Copy-Item -Force $DotDir\user.js $Dest
+    if (Test-Path -Path "$Dest\chrome") {
+        Get-ChildItem -Path $Dest\chrome *.css -Recurse | ForEach-Object { Remove-Item -Path $_.FullName }
+    }
+    ":: Install css tweaks to ${Target}"
+    Copy-Item -Force -Recurse $DotDir\chrome $Dest
+}
+
 function Dot-Install {
-    $FirefoxDir = "$env:APPDATA\Mozilla\Firefox"
-    if (!(Test-Path $FirefoxDir\profiles.ini)) {
+    if (!(Test-Path $FFDir\profiles.ini)) {
         "no firefox profile detected, open firefox first before restoring tweaks"
         exit
     }
-    Get-IniContent -filePath $FirefoxDir\profiles.ini | ForEach-Object {$_[$_.Keys].Path} | ForEach-Object {
-        if (Test-Path "$FirefoxDir\${_}\prefs.js") {
-            ":: Install user.js to ${_}"
-            Copy-Item -Force $PSScriptRoot\user.js "$FirefoxDir\${_}"
+    Get-IniContent -filePath $FFDir\profiles.ini | ForEach-Object { $_[$_.Keys].Path } | ForEach-Object {
+        if (Test-Path "$FFDir\${_}\prefs.js") {
+            Install-Files $PSScriptRoot $_
         }
     }
 }
