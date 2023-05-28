9ad15d7fd20f5d3d0ebfac29e79e99ae70dd30a9
Nauval Rizky <enuma.alrizky@gmail.com>
2023-05-28 23:37:54 +0700

    nvim: add cmp for nvim lua api

--- nvim/lua/nxc/lazy.lua
+++ nvim/lua/nxc/lazy.lua
@@ -58,6 +58,7 @@ require("lazy").setup({
             'hrsh7th/cmp-buffer',
             'hrsh7th/cmp-path',
             'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-nvim-lua',
         }
     },
     {
