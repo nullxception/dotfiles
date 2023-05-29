3d2b830f0905f6680371821f304e5fc849edd2d0
Nauval Rizky <enuma.alrizky@gmail.com>
2023-05-29 19:23:02 +0700

    nvim: update kurukuru

--- nvim/after/plugin/dashboard.lua
+++ nvim/after/plugin/dashboard.lua
@@ -4,13 +4,13 @@ if not success then
 end
 
 local utils = require("nxc.utils")
-local hesta_img = require("nxc.ascii.hesta")
+local hesta = require("nxc.ascii.hesta")
 
 require("dashboard").setup({
     theme = 'doom',
     config = {
         header = utils.build_header({
-            data = hesta_img,
+            data = hesta.normal,
             padding_top = 1,
             padding_bottom = 2,
         }),
