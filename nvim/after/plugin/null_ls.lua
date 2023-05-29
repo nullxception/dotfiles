335c8b5b89fd8d8a550eebe1c6b90a6fa35ad978
Nauval Rizky <enuma.alrizky@gmail.com>
2023-05-29 22:54:23 +0700

    nvim: add more stuff to null-ls

--- nvim/after/plugin/null_ls.lua
+++ nvim/after/plugin/null_ls.lua
@@ -7,6 +7,7 @@ local null_ls = require("null-ls")
 null_ls.setup({
     sources = {
         null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.shfmt,
         null_ls.builtins.diagnostics.eslint,
     }
 })
