7e9c55b51350d13693e31784b65925903fdfa190
Nauval Rizky <enuma.alrizky@gmail.com>
2025-09-28 04:37:12 +0700

    move powershell setup to pwsh

--- windows/pwsh/.install.ps1
+++
@@ -1,8 +0,0 @@
-$InstallTarget = Split-Path $profile
-
-function dot_postinstall {
-    if (!(Get-InstalledModule posh-git -ErrorAction SilentlyContinue)) {
-        Install-Module posh-git -Scope CurrentUser -Force
-    }
-    Update-Module posh-git
-}
