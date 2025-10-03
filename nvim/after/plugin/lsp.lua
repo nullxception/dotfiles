79d445f666ad683b0e88b8ce171bae923fa32aef
Nauval Rizky <enuma.alrizky@gmail.com>
2025-10-03 16:20:57 +0700

    nvim: migrate from cmp-nvim-lsp to blink.cmp

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,17 +1,8 @@
-local real_create = vim.lsp.client.create
-
 vim.diagnostic.config({
     virtual_text = false,
     virtual_lines = true,
     update_in_insert = true,
 })
-
-vim.lsp.client.create = function(config)
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    return real_create(vim.tbl_deep_extend("keep", config, {
-        capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities),
-    }))
-end
 
 vim.lsp.config.lua_ls = {
     settings = {
