079f276e79724213a12e84e7dfb5e09a20579d64
Nauval Rizky <enuma.alrizky@gmail.com>
2025-10-08 03:43:27 +0000

    zsh: merge back into .zshrc and .zshenv

--- zsh/.zsh/after/ccache.zsh
+++
@@ -1,9 +0,0 @@
-command -v ccache >/dev/null || return 0
-
-# configure ccache
-export USE_CCACHE=1
-export CCACHE_EXEC=$(which ccache)
-export CCACHE_DIR="$XDG_CACHE_HOME/ccache"
-export CCACHE_MAX_FILES=0
-export CCACHE_MAX_SIZE=100G
-ccache -M ${CCACHE_MAX_SIZE:=50G} -F ccache ${CCACHE_MAX_FILES:=0} >/dev/null 2>&1
