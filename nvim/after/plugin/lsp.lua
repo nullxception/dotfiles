17ef67c077a0e9e7601370cb3a87f43208805914
Nauval Rizky <enuma.alrizky@gmail.com>
2025-10-15 04:46:23 +0700

    nvim: setup powershell_es lsp for external usage

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -30,3 +30,18 @@ vim.lsp.config.jsonls = {
         },
     },
 }
+
+vim.lsp.config.powershell_es = {
+    filetypes = { "ps1", "psm1", "psd1" },
+    bundle_path = vim.fn.expand("$MASON/packages/powershell-editor-services"),
+    settings = {
+        powershell = {
+            codeFormatting = {
+                Preset = "OTBS",
+            },
+        },
+    },
+    init_options = {
+        enableProfileLoading = false,
+    },
+}
