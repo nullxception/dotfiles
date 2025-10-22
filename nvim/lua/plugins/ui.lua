d443b70b8f25f14cd9f1b917d3617b5f31c28980
Nauval Rizky <enuma.alrizky@gmail.com>
2025-10-23 06:07:28 +0700

    nvim: add lsp progress notification

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -5,6 +5,7 @@ return {
     {
         "folke/snacks.nvim",
         priority = 1000,
+        lazy = false,
         ---@module "snacks"
         ---@type snacks.Config
         opts = {
@@ -17,6 +18,22 @@ return {
                 win = { height = 0.25 },
             },
         },
+        init = function()
+            vim.api.nvim_create_autocmd("LspProgress", {
+                ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
+                callback = function(ev)
+                    local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+                    vim.notify(vim.lsp.status(), "info", {
+                        id = "lsp_progress",
+                        title = "LSP Progress",
+                        opts = function(notif)
+                            notif.icon = ev.data.params.value.kind == "end" and " "
+                                or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+                        end,
+                    })
+                end,
+            })
+        end,
         keys = {
             {
                 "<A-`>",
