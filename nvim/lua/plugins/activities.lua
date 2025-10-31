6d246b4666080fd7ba0091e7d558c7dec337fe9e
Nauval Rizky <enuma.alrizky@gmail.com>
2025-10-31 21:49:23 +0700

    nvim: add wakatime and cord.nvim

---
+++ nvim/lua/plugins/activities.lua
@@ -0,0 +1,26 @@
+vim.pack.add({
+    gh("wakatime/vim-wakatime"),
+    gh("vyfor/cord.nvim"),
+}, { confirm = false, load = true })
+
+local home = vim.env.HOME or os.getenv("HOME")
+local cfg = vim.fs.joinpath(home, ".wakatime.cfg")
+if vim.uv.fs_stat(cfg) ~= nil then
+    require("wakatime").setup({
+        plugin_name = "neovim-wakatime", -- fix editor = 'unknown' issues on wakapi
+        heartbeat_frequency = 1,
+    })
+end
+
+vim.api.nvim_create_autocmd("PackChanged", {
+    callback = function(opts)
+        if opts.data.spec.name == "cord.nvim" and opts.data.kind == "update" then
+            vim.cmd("Cord update")
+        end
+    end,
+})
+require("cord").setup({
+    text = {
+        workspace = "In /Library/****",
+    },
+})
