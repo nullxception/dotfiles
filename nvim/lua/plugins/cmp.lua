59b7874884d257a7410405951b61a0542f8b5ced
Nauval Rizky <enuma.alrizky@gmail.com>
2025-11-01 20:24:47 +0700

    nvim: only load lazydev when opening lua file

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -4,11 +4,19 @@ vim.pack.add({
     { src = gh("saghen/blink.cmp"), version = vim.version.range("1.*") },
 }, { confirm = false })
 
-require("lazydev").setup({
-    library = {
-        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-        { path = "wezterm-types", mods = { "wezterm" } },
-    },
+local augroup = vim.api.nvim_create_augroup("UserCmpAuto", { clear = true })
+vim.api.nvim_create_autocmd("BufEnter", {
+    once = true,
+    group = augroup,
+    pattern = "*.lua",
+    callback = function()
+        require("lazydev").setup({
+            library = {
+                { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                { path = "wezterm-types", mods = { "wezterm" } },
+            },
+        })
+    end,
 })
 
 require("blink.cmp").setup({
